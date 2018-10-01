52d3172f1e3b83353593700ed316e688e02a3509
gescobar-technisys <gescobar@technisys.com>
2018-10-01 16:23:00 -0300

    Se crea formulario para el ingreso de pacientes

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import AgregarCita from './AgregarCita';
+import ListaCistas from './ListaCitas';
 
 class App extends Component {
+  
+  state = {
+    citas: {}
+
+  }
+
+  crearCita = infoCita => {
+    //toma una copia del state
+    const citas = {...this.state.citas};
+
+    //lo agrego al state actual
+    citas[`citas${Date.now()}`] = infoCita;
+    
+    //Set state
+    this.setState({
+      citas
+    })
+  }
+
+  borrarCita = id => {
+    console.log('Eliminando...' + id);
+  }
+
   render() {
     return (
       <div className = "container">
         <Header 
         titulo = 'Administrador de pacientes de veterinaria'
         />
+
+        <div className = "row">
+          <div className="col-md-6">
+            <AgregarCita
+              crearCita={this.crearCita}
+            />
+          </div>
+          <div className="col-md-6">
+            <ListaCistas
+              citas={this.state.citas}
+              borrarCita={this.borrarCita}
+            />
+          </div>
+        </div>
       </div>
     );
   }
